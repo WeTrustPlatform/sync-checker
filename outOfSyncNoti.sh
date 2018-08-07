0e0769742e724adc772ac6bcf707cfdc2d2aa27a
Jean-André Santoni <jean.andre.santoni@gmail.com>
2018-08-07 16:17:10 +0700

    serviceName

--- outOfSyncNoti.sh
+++ outOfSyncNoti.sh
@@ -2,13 +2,14 @@
 # A simple shell script to check if node is out of sync with ethereum
 # If node is out of sync, user will be notified by OpsGenie
 # Using crontab command:
-# */10 * * * * /home/foo/outOfSyncNoti.sh 'rinkeby' '8545' 'opsgeniekeyhere' >> /home/foo/nodeSync.log 2>&1`
+# */10 * * * * /home/foo/outOfSyncNoti.sh rinkeby 8545 opsgeniekeyhere geth.service >> /home/foo/nodeSync.log 2>&1`
 # to run this script every 10 mins.
 
 network=$1           # ethereum network in letters
 port=$2              # local geth port
 genieKey=$3          # opsgenie key
-threshold=${4:-200}  # number of blocks behind
+serviceName=$4       # name of the systemd service to restart
+threshold=${5:200}   # number of blocks behind
 
 # Get current node block number
 curBlock=$(geth --exec "eth.blockNumber" attach http://localhost:${port})
@@ -30,7 +31,7 @@ echo "latest block number from localhost:${port} : $curBlock"
 # Check if node is out of sync.
 # Will send notification when current block number is <threshold> blocks behind latest block on etherscan.
 if [ $(( ${ethBlock} - ${curBlock} )) -ge $threshold ]; then
-  systemctl restart geth
+  systemctl restart ${serviceName}
   curl -X POST https://api.opsgenie.com/v2/alerts -H "Content-Type: application/json" -H "Authorization: GenieKey $genieKey" -d '{ "message": "Our block is out of sync." }'
   echo ""
 fi
